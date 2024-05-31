ca5a7c4961db4dc4833d132056d1ee705a1cdbd6
winkerVSbecks <varunvachhar@gmail.com>
2024-05-31 17:32:31 -0400

    prep for chromatic demo

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress'
+const { installPlugin } = require('@chromatic-com/cypress')
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      installPlugin(on, config)
+      // implement node event listeners here
+    },
+  },
+})
