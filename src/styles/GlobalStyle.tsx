437bae7771c6db4a8b663a95e7583a8253a180e5
Sukaina Bahsoun <suki@chromatic.com>
2025-04-03 09:39:14 -0500

    adjusting styles

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -77,7 +77,7 @@ export const GlobalStyle = createGlobalStyle(
       src:
         local('Hind Regular'),
         local('Hind-Regular'),
-        url(https://fonts.gstatic.com/s/hind/v11/5aU69_a8oxmIdGl4BDGwgDI.woff2) format('woff2');
+        url(https://fontss.gstatic.com/s/hind/v11/5aU69_a8oxmIdGl4BDGwgDI.woff2) format('woff2');
       unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC,
         U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
     }
