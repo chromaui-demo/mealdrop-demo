4ed87d6f62f523e759b74d2827e35fab3d39b2fb
Sukaina Bahsoun <suki@chromatic.com>
2025-09-17 08:18:02 -0500

    changing color

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -59,12 +59,12 @@ export const IconAndText: Story = {
 
 export const LongText: Story = {
   args: {
-    children: 'Buttonnnnnnssssssssssssssssssssssss',
+    children: 'Buttonnnnnnsssssssssssssssssssss',
     round: true,
   },
 
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement.ownerDocument.body);
-    await userEvent.click(await canvas.findByRole('button', { name: 'Buttonnnnnnssssssssssssssssssssssss' }));
+    await userEvent.click(await canvas.findByRole('button', { name: 'Buttonnnnnnsssssssssssssssssssss' }));
   }
 }
